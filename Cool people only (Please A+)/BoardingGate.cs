da20d42fb024c7b9093261fdbe352e4a242afdfc
Jun Wei Yap <s10259029@connect.np.edu.sg>
2025-01-20 18:25:11 +0800

    Empty Files

---
+++ Cool people only (Please A+)/BoardingGate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cool_people_only__Please_A__
+{
+    internal class BoardingGate
+    {
+    }
+}

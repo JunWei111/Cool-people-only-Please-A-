78a6029ea6de1ef96210eef995f4b3dc798e6653
Jun Wei Yap <s10259029@connect.np.edu.sg>
2025-01-22 18:20:25 +0800

    changed menu

--- Cool people only (Please A+)/Program.cs
+++ Cool people only (Please A+)/Program.cs
@@ -14,77 +14,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 void DisplayMenu()
 {
-    bool exit = false;
-    while (!exit)
-    {
-        try
-        {
-            Console.WriteLine("=============================================");
-            Console.WriteLine("Welcome to Changi Airport Terminal 5");
-            Console.WriteLine("=============================================");
-            Console.WriteLine("1. List All Flights");
-            Console.WriteLine("2. List Boarding Gates");
-            Console.WriteLine("3. Assign a Boarding Gate to a Flight");
-            Console.WriteLine("4. Create Flight");
-            Console.WriteLine("5. Display Airline Flights");
-            Console.WriteLine("6. Modify Flight Details");
-            Console.WriteLine("7. Display Flight Schedule");
-            Console.WriteLine("0. Exit");
-            Console.WriteLine("");
-            Console.WriteLine("Please select your option:");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            if (choice >= 0 && choice <= 7)
-            {
-                switch (choice)
-                {
-                    case 1:
-                        //DisplayFlight(terminal);
-                        continue;
-
-                    case 2:
-                        //ListBoardingGates(boardingGates);
-                        continue;
-
-                    case 3:
-
-                        continue;
-
-                    case 4:
-
-                        continue;
-
-                    case 5:
-
-                        continue;
-
-                    case 6:
-
-                        continue;
-
-                    case 7:
-
-                        continue;
-
-                    case 0:
-                        Console.WriteLine("Goodbye!");
-                        exit = true; // Set to true to break out of the loop
-                        break;
-
-                    default:
-                        break;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid option. Please choose a valid option between 0 and 7.");
-            }
-        }
-        catch (FormatException)
-        {
-            Console.WriteLine("Invalid input. Please enter a number between 0 and 7.");
-        }
-    }
-
+    Console.WriteLine("=============================================");
+    Console.WriteLine("Welcome to Changi Airport Terminal 5");
+    Console.WriteLine("=============================================");
+    Console.WriteLine("1. List All Flights");
+    Console.WriteLine("2. List Boarding Gates");
+    Console.WriteLine("3. Assign a Boarding Gate to a Flight");
+    Console.WriteLine("4. Create Flight");
+    Console.WriteLine("5. Display Airline Flights");
+    Console.WriteLine("6. Modify Flight Details");
+    Console.WriteLine("7. Display Flight Schedule");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine("");
 }
 
 //Task 1
